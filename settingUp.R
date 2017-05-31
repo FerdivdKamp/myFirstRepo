c3f10c5f819e4b640684a90e06c220f32a07760f
Kamp <kampfcvande@vuw.leidenuniv.nl>
2017-05-31 12:37:13 +0200

    Setting up a package "Pack"

---
+++ settingUp.R
@@ -0,0 +1,13 @@
+# Setting up a package
+install.packages("Rtools")
+install.packages("devtools")
+library("devtools")
+devtools::install_github("klutometis/roxygen") # will not work in LU, but can still continue working on the package
+library(roxygen2) # will not work in LU, but can still continue working on the package
+
+
+# Test Sys path for Rtools
+Sys.getenv("PATH")
+
+getwd()
+create("pack")
