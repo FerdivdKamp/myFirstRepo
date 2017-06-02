6db7dcb5f15bb9ed3679822f8f916732a5618dfa
FerdivdKamp <ferdivdkamp@gmail.com>
2017-06-02 10:00:24 +0200

    Working function documentation

--- pack/R/QAtool.R
+++ pack/R/QAtool.R
@@ -3,16 +3,16 @@
 #' Applies a prebuilt model to Freesurfer output for QA
 #' returns classification
 #' @param input Can be .r .txt or .csv
-#' output Is gonna be quite something
-#' model 'general' or 'personalised' Default is 'general'
+#' @param output Is gonna be quite something
+#' @param model 'general' or 'personalised' Default is 'general'
 #' @examples QAtool('FreesurferData.csv', 'QAdone.csv', model='general') )
 #' QAtool()
 
 QAtool = function(input, output, model='general'){
   data(input) # load data (works for .r, .txt, .csv)
-  if model=='general'
+  if(model=='general'){
     classifier = readRDS('classifier.rds') # load model
     y_pred = predict(classifier, input) # apply model to input
     # output to be entered
-   
+  }
 }
--- settingUp.R
+++ settingUp.R
@@ -1,8 +1,13 @@
+#install.packages('installr')
+#library(installr)
+#updateR()
+
 # Setting up a package
 install.packages("Rtools")
 install.packages("devtools")
 library("devtools")
-devtools::install_github("klutometis/roxygen") # will not work in LU, but can still continue working on the package
+#devtools::install_github("klutometis/roxygen") # will not work in LU, but can still continue working on the package
+install.packages('roxygen2')
 library(roxygen2) # will not work in LU, but can still continue working on the package
 
 
@@ -12,4 +17,10 @@ Sys.getenv("PATH")
 getwd()
 create("pack")
 
+setwd('./pack')
+
+document()
+setwd('..')
 install("pack")
+
+?QAtool
